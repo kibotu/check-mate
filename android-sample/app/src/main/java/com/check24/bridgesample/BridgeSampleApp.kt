1a5ddf6cc408337ad48e9e722689b2b39a42b635
Jan Rabe <cloudgazer3d@gmail.com>
2025-10-20 16:30:03 +0200

    added timber

---
+++ android-sample/app/src/main/java/com/check24/bridgesample/BridgeSampleApp.kt
@@ -0,0 +1,21 @@
+package com.check24.bridgesample
+
+import android.app.Application
+import android.content.pm.ApplicationInfo
+import timber.log.Timber
+
+class BridgeSampleApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val isDebuggable = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
+        if (isDebuggable) {
+            Timber.plant(Timber.DebugTree())
+            android.webkit.WebView.setWebContentsDebuggingEnabled(true)
+        }
+        Timber.i("BridgeSampleApp initialized")
+    }
+}
+
+
